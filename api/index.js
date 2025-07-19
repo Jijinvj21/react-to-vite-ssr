1b48e1b1e5f6691d98666833f42a7dd1044fceec
Jijinvj21 <jijinvjvazhippilly@gmail.com>
2025-07-19 16:23:05 +0530

    change all thing

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// This file is required for Vercel to recognize the serverless function
-// but we'll handle routing in server.js
-export default (req, res) => {
-  res.status(404).send("Not found");
-};
