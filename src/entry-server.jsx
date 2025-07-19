d03d141ac98660a079907bc077b8277fdb65630b
Jijinvj21 <jijinvjvazhippilly@gmail.com>
2025-07-19 16:59:56 +0530

    package change

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,35 +1,4 @@
-// import { renderToString } from "react-dom/server";
-// import { StaticRouter } from "react-router-dom/server";
-// import { HelmetProvider } from "react-helmet-async";
-// import App from "./App";
 
-// export function render(url, metadata) {
-//   const location = url.startsWith("http") ? new URL(url).pathname : url;
-//   const helmetContext = {};
-
-//   const appHtml = renderToString(
-//     <HelmetProvider context={helmetContext}>
-//       <StaticRouter location={location}>
-//         <App metadata={metadata} />
-//       </StaticRouter>
-//     </HelmetProvider>
-//   );
-
-//   const { helmet } = helmetContext;
-
-//   return `
-//     <!DOCTYPE html>
-//     <html lang="en">
-//       <head>
-//         ${helmet.title.toString()}
-//         ${helmet.meta.toString()}
-//       </head>
-//       <body>
-//         <div id="root">${appHtml}</div>
-//       </body>
-//     </html>
-//   `;
-// }
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import App from "./App";
