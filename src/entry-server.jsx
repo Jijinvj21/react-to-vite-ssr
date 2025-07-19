3611c048d7e81005960fe2b72a000863d9c1caaf
Jijinvj21 <jijinvjvazhippilly@gmail.com>
2025-07-19 16:54:15 +0530

    change all thing

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,32 +1,46 @@
+// import { renderToString } from "react-dom/server";
+// import { StaticRouter } from "react-router-dom/server";
+// import { HelmetProvider } from "react-helmet-async";
+// import App from "./App";
+
+// export function render(url, metadata) {
+//   const location = url.startsWith("http") ? new URL(url).pathname : url;
+//   const helmetContext = {};
+
+//   const appHtml = renderToString(
+//     <HelmetProvider context={helmetContext}>
+//       <StaticRouter location={location}>
+//         <App metadata={metadata} />
+//       </StaticRouter>
+//     </HelmetProvider>
+//   );
+
+//   const { helmet } = helmetContext;
+
+//   return `
+//     <!DOCTYPE html>
+//     <html lang="en">
+//       <head>
+//         ${helmet.title.toString()}
+//         ${helmet.meta.toString()}
+//       </head>
+//       <body>
+//         <div id="root">${appHtml}</div>
+//       </body>
+//     </html>
+//   `;
+// }
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 
 export function render(url, metadata) {
+  // Use a simple path if there are issues
   const location = url.startsWith("http") ? new URL(url).pathname : url;
-  const helmetContext = {};
-
-  const appHtml = renderToString(
-    <HelmetProvider context={helmetContext}>
-      <StaticRouter location={location}>
-        <App metadata={metadata} />
-      </StaticRouter>
-    </HelmetProvider>
+  
+  return renderToString(
+    <StaticRouter location={location}>
+      <App metadata={metadata} />
+    </StaticRouter>
   );
-
-  const { helmet } = helmetContext;
-
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        ${helmet.title.toString()}
-        ${helmet.meta.toString()}
-      </head>
-      <body>
-        <div id="root">${appHtml}</div>
-      </body>
-    </html>
-  `;
 }
